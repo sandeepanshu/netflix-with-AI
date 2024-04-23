138eeee0d23e34208570c11fbac36db900985114
Sandeep kumar Sharma <57533185+Sandeep9199@users.noreply.github.com>
2024-04-24 00:56:32 +0530

    Updating files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -57,7 +57,7 @@ const Header = () => {
     dispatch(changeLanguage(e.target.value));
   };
   return (
-    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between ">
       <img className="w-44" src={LOGO} alt="logo" />
 
       {user && (
