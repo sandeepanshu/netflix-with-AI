f8f62644124394382920540bf8e2fc245219dec8
Sandeep kumar Sharma <sandeepks9199@gmail.com>
2025-09-07 20:16:47 +0530

    New change

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,24 +1,29 @@
 import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constant";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
-import { useEffect, useCallback } from "react";
+import { useEffect } from "react";
 
 const useNowPlayingMovies = () => {
+  // fetch the ata from TMDB and update the store
   const dispatch = useDispatch();
-  const nowPlayingMovies = useSelector((store) => store.movies.nowPlayingMovies);
+  
+  const nowPlayingMovies = useSelector(
+    (store) => store.movies.nowPlayingMovies
+  );
 
-  const getNowPlayingMovie = useCallback(async () => {
+  const getNowPlayingMovie = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?page=1",
       API_OPTIONS
     );
     const json = await data.json();
     dispatch(addNowPlayingMovies(json.results));
-  }, [dispatch]);
+  };
 
   useEffect(() => {
-    if (!nowPlayingMovies) getNowPlayingMovie();
-  }, [nowPlayingMovies, getNowPlayingMovie]);
+    // if(!nowPlayingMovies) getNowPlayingMovie();
+    !nowPlayingMovies && getNowPlayingMovie();
+  }, []);
 };
 
 export default useNowPlayingMovies;
