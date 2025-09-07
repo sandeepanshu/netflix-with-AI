84d789ff8e5ca68c385bf03cda159da8399dc19c
Sandeep kumar Sharma <sandeepks9199@gmail.com>
2025-09-07 20:12:10 +0530

    ES lint fixed

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,29 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constant";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 
 const useNowPlayingMovies = () => {
-  // fetch the ata from TMDB and update the store
   const dispatch = useDispatch();
-  
-  const nowPlayingMovies = useSelector(
-    (store) => store.movies.nowPlayingMovies
-  );
+  const nowPlayingMovies = useSelector((store) => store.movies.nowPlayingMovies);
 
-  const getNowPlayingMovie = async () => {
+  const getNowPlayingMovie = useCallback(async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?page=1",
       API_OPTIONS
     );
     const json = await data.json();
     dispatch(addNowPlayingMovies(json.results));
-  };
+  }, [dispatch]);
 
   useEffect(() => {
-    // if(!nowPlayingMovies) getNowPlayingMovie();
-    !nowPlayingMovies && getNowPlayingMovie();
-  }, []);
+    if (!nowPlayingMovies) getNowPlayingMovie();
+  }, [nowPlayingMovies, getNowPlayingMovie]);
 };
 
 export default useNowPlayingMovies;
